602b382afcae55a929522d7fb69af75d1699cd1c
Haleniuk.M <galenukm@gmail.com>
2026-01-13 22:23:00 +0200

    Migration EditTableMovie
    Add field Actors
    Edit field name Poster to PosterUrl

--- CinemaProject/CinemaProject.Server/Models/Entitys/Movie.cs
+++ CinemaProject/CinemaProject.Server/Models/Entitys/Movie.cs
@@ -4,9 +4,10 @@
     {
         public int MovieId { get; set; }
         public string Title { get; set; } = null!;
-        public int Duration { get; set; }
-        public string? Description { get; set; }
-        public string Poster { get; set; } = null!;
+        public int Duration { get; set; } 
+        public string? Description { get; set; } = null!;
+        public string PosterUrl { get; set; } = null!;
+        public string? Actors { get; set; } = null!;
 
         public ICollection<MovieGenre> MovieGenres { get; set; } = new List<MovieGenre>();
         public ICollection<Session> Sessions { get; set; } = new List<Session>();
