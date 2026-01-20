bab437cc3faf76b6fc29dca0f7dbff86fd7b22ac
Haleniuk.M <galenukm@gmail.com>
2026-01-20 17:37:04 +0200

    feat: configure Swagger for API

--- CinemaProject/CinemaProject.Server/Program.cs
+++ CinemaProject/CinemaProject.Server/Program.cs
@@ -5,35 +5,45 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 
+// DbContext
 builder.Services.AddDbContext<CinemaDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin() 
+        policy.AllowAnyOrigin()
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
 });
 
+// HTTP Client
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IMovieService, MovieService>();
 
+// Swagger / OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CinemaProject.Server | v1");
+    });
+}
 
 app.UseCors();
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
